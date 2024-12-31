276dc95994f3fef633c38090b9c26a88627062ea
Chenyu Zheng <chnyzheng@gmail.com>
2024-12-31 17:18:00 -0500

    changed calculator

--- C/calcProject/basicCalculator.c
+++ C/calcProject/basicCalculator.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "linkedlist.h"
 
 #define MAX_LENGTH 100
 
@@ -13,60 +14,12 @@ void getEq(char *eqString){
     fgets(eqString, MAX_LENGTH, stdin);
 }
 
-void tokenize(const char *equation){
-    char *token;
-    char input[MAX_LENGTH];
-
-    token = strtok(input, " ");
-    while(token!=NULL){
-        printf("Token: %s\n", token);
-        token = strtok(NULL, " ");
-    }
-}
-
-int basicCalculator(const char *equation){
-    char *token;
-    char input[MAX_LENGTH];
-    
-    int total = 0;
-    char prevToken;
-
-    strncpy(input, equation, MAX_LENGTH);
-
-    token = strtok(input, " ");
+int Calculator(const char *equation){
+    //how we want this to work... we need to take the equation and
+    //set priority based on the PEMDAS
 
 
-    while(token!=NULL){
-        if(strcmp(token, "+")!=0&&strcmp(token, "-")!=0){
-            //seperate out the numbers...
-            int num = atoi(token);
-            if(prevToken == '+'){
-                total = total + num;
-            }
-            else if(prevToken == '-'){
-                total = total - num;
-            }
-            else{
-                total = num;
-            }
 
 
-        }
-        else{
-            prevToken = *token;
-        }
-        token = strtok(NULL, " ");
-    }
-
-
-    return total;
-}
-
-int main(){
-    char equation[MAX_LENGTH]; //max of 100
-    getEq(equation);
-    int value = basicCalculator(equation);
-    printf("%d", value);
-    return 0;
 }
 
